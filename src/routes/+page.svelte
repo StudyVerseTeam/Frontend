546a947c1c9d8f6f40063d70ef76e7d02e042757
vibovenkat123 <vibovenkat@gmail.com>
2022-11-21 12:23:26 -0600

    rewrite in svelte easier for state management, apis, interactiveness and everything

---
+++ src/routes/+page.svelte
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8" />
+<meta
+  name="viewport"
+  content="width=device-width, initial-scale=1, user-scalable=no"
+/>
+<link rel="stylesheet" href="/css/main.css" />
+<noscript><link rel="stylesheet" href="/css/noscript.css" type="text/css"/></noscript>
+<head>
+  <body class="is-preload">
+    <!-- Wrapper -->
+    <div id="wrapper">
+      <!-- Header -->
+      <header id="header" class="alt">
+        <a href="/" class="logo">
+          <strong>Study</strong> <span>Verse</span></a>
+        <nav>
+          <a href="#menu">Menu</a>
+        </nav>
+      </header>
+
+      <!-- Menu -->
+      <nav id="menu">
+        <ul class="links">
+          <li><a href="/page">Page template</a></li>
+          <li><a href="/page2">Page template</a></li>
+        </ul>
+        <ul class="actions stacked">
+          <li><a href="/signup" class="button primary fit">Get Started</a></li>
+          <li><a href="/login" class="button fit">Log In</a></li>
+        </ul>
+      </nav>
+
+      <!-- Banner -->
+      <section id="banner" class="major">
+        <div class="inner">
+          <header class="major">
+            <h1>Study Verse</h1>
+          </header>
+          <div class="content">
+            <p>
+              An all in one learning platform that extends student's minds and improves their organization skills.
+            </p>
+            <ul class="actions">
+              <li>
+                <a href="#one" class="button next scrolly">See the features</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
+
+      <!-- Main -->
+      <div id="main">
+        <!-- One -->
+        <section id="one" class="tiles">
+          <!-- Tile 1-->
+          <article>
+            <span class="image">
+              <img src="images/pic01.jpg" alt="" />
+            </span>
+            <header class="major">
+              <h3><a href="/signup" class="link">Organize your life</a></h3>
+              <p>Organize your life by using the built in To-Do app, and agenda manager (both coming soon). No more needing to find seperate apps for school, everything is (almost) already here on StudyVerse.</p>
+            </header>
+          </article>
+          <!-- Tile 2-->
+          <article>
+            <span class="image">
+              <img src="images/pic02.jpg" alt="" />
+            </span>
+            <header class="major">
+              <h3><a href="/signup" class="link">Grades</a></h3>
+              <p>Stop procrastinating, start doing. Get motivated to study by gaining awards and reputation (coming soon). You can flex on your peers all while maintaining good grades.</p>
+            </header>
+          </article>
+          <!-- Tile 3-->
+          <article>
+							<span class="image">
+								<img src="images/pic03.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h3><a href="/signup" class="link">Help Others</a></h3>
+								<p>Enhance your teaching skills! Create planets (Study Sets) (coming soon) and share them with friends. You gain awards and reputation whenever someone plays it!</p>
+							</header>
+					</article>
+          <!-- Tile 4-->
+          <article>
+							<span class="image">
+								<img src="images/pic04.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h3><a href="/signup" class="link">Work Together</a></h3>
+								<p>Work together with your friends by creating a customized calendar schedule (coming soon). It can be for studying times, playing games, and pretty much everything else.</p>
+							</header>
+					</article>
+          <!-- Tile 5-->
+          <article>
+							<span class="image">
+								<img src="images/pic05.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h3><a href="/signup" class="link">Galaxies</a></h3>
+                <p>Take your teaching game to the next level with galaxies (courses) (coming soon). Galaxies are bundles of planets combined with stars (lectures/typed papers) to help enhance student's skills.</p>
+							</header>
+					</article>
+          <!-- Tile 6-->
+          <article>
+							<span class="image">
+								<img src="images/pic06.jpg" alt="" />
+							</span>
+							<header class="major">
+								<h3><a href="/signup" class="link">Meteors</a></h3>
+								<p>Make graded assignments with Meteors (assignments) (coming soon). Monotoring your students (or friends) progress is a great way to gain understanding and provide for them better.</p>
+							</header>
+					</article>
+        </section>
+
+        <!-- Two -->
+      </div>
+
+      <!-- Contact -->
+
+      <!-- Footer -->
+      <footer id="footer">
+        <div class="inner">
+          <ul class="copyright">
+            <li></li>
+            <li>
+              <a href="#">
+                PLACE HOLDER Give copyright credit here
+              </a>
+            </li>
+          </ul>
+        </div>
+      </footer>
+    </div>
+
+    <!-- Scripts -->
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/jquery.scrolly.min.js"></script>
+    <script src="/js/jquery.scrollex.min.js"></script>
+    <script src="/js/browser.min.js"></script>
+    <script src="/js/breakpoints.min.js"></script>
+    <script src="/js/util.js"></script>
+    <script src="/js/main.js"></script>
+    <script src="/js/icon.js" defer></script>
+  </body>
+</head>
+</head>
---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Login page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.9/typicons.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css"><link rel="stylesheet" href="css/login.css">
+<script src="/js/icon.js" defer></script>
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<body id="particles-js"></body>
+<div class="animated bounceInDown">
+  <div class="container">
+    <span class="error animated tada" id="msg"></span>
+    <form name="form1" class="box" onsubmit="return checkStuff()">
+     <a href="/" style="text-decoration: none;">
+       <h4>Study<span>Verse</span></h4>
+     </a>
+      <h5>Sign up</h5>
+        <input type="name" name="name" placeholder="Name" autocomplete="off">
+        <input type="password" name="password" placeholder="Password" id="pwd" autocomplete="off">
+        <i class="typcn typcn-eye" id="eye"></i>
+        <input type="text" name="email" placeholder="Email" autocomplete="off">
+        <label>
+          <input type="checkbox">
+          <span></span>
+          <small class="rmb">Remember me</small>
+        </label>
+        <input type="submit" value="Sign up" class="btn1">
+      </form>
+    <a href="/login" class="dnthave">Already have an account? Sign in.</a>
+  </div> 
+  </div> 
+<!-- partial -->
+  <script src="https://cldup.com/S6Ptkwu_qA.js"></script><script  src="js/input.js"></script>
+
+</body>
+</html>
+
