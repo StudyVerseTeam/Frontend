4d54a49a890033d189eb3f9b4da702048c6edc98
vibovenkat123 <vibovenkat@gmail.com>
2022-11-23 11:41:41 -0600

    add decoding

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -4,17 +4,31 @@ export const actions = {
     default: async ({request}:any) => {
         // get data
         const formData = await request.formData();
+        console.log(import.meta.env.VITE_API_URL)
         // get the resulta
+        console.log(encodeURIComponent(formData.get('password')))
+        console.log(encodeURIComponent(formData.get('email')))
         const res = await fetch(`${import.meta.env.VITE_API_URL}/api/login?email=${encodeURIComponent(formData.get('email'))}&password=${encodeURIComponent(formData.get('password'))}`)
         // get the json
-        const data = await res.json()
+        const raw = await res.json()
+        let data = raw
+        for (let i =0; i < raw.length; i ++) {
+          if ( raw[i] == "email" || raw[i] == "pass" || raw[i] == "name") {
+              data[i] = decodeURIComponent(raw[i]) 
+          } else {
+            data[i] = raw[i]
+          }
+        }
+        console.log(data)
         // make the user store data
         // if the user exists
         if (data.exists) {
           user.set(data)
             //redirect them
             throw redirect(303, '/')
-        } else if (data.error !== null) {
+        } else if (!data.exists) {
+          return {error: false, msg: "doesnt exist"}
+        } else  {
           return {error: true, msg: data.error}
         }
     }
