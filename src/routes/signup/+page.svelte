bcc0c5a5d731d0f4fbda48757d337e0e7b0e3e3e
vibovenkat <vibovenkat@gmail.com>
2022-11-26 05:02:52 +0000

    add check

--- src/routes/signup/+page.svelte
+++ src/routes/signup/+page.svelte
@@ -113,7 +113,10 @@
   <div class="container">
     {#if form?.email}
       <div style="padding: 2em;">
-        <img src="/images/favicon.ico" width="50" height="50" alt = "logo"/>
+        <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+          <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+          <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+        </svg>
       <h3 style="color:white;"> You are almost done!</h3>
       <p style="color:white;"> You are almost done <strong>{form?.email}</strong>, now all you have to do is verify your email.</p>
       <p style="color:white;"> Check your inbox for an email with a link to verify, remember to check your spam folder!</p>
@@ -148,4 +151,52 @@
   .container {
     height: 400px;
   }
+  .checkmark__circle {
+  stroke-dasharray: 166;
+  stroke-dashoffset: 166;
+  stroke-width: 2;
+  stroke-miterlimit: 10;
+  stroke: #7ac142;
+  fill: none;
+  animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+}
+
+.checkmark {
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  display: block;
+  stroke-width: 2;
+  stroke: #fff;
+  stroke-miterlimit: 10;
+  margin: 10% auto;
+  box-shadow: inset 0px 0px 0px #7ac142;
+  animation: fill .4s ease-in-out .4s forwards, scale .3s ease-in-out .9s both;
+}
+
+.checkmark__check {
+  transform-origin: 50% 50%;
+  stroke-dasharray: 48;
+  stroke-dashoffset: 48;
+  animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+}
+
+@keyframes stroke {
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+@keyframes scale {
+  0%, 100% {
+    transform: none;
+  }
+  50% {
+    transform: scale3d(1.1, 1.1, 1);
+  }
+}
+@keyframes fill {
+  100% {
+    box-shadow: inset 0px 0px 0px 30px #7ac142;
+  }
+}
 </style>
