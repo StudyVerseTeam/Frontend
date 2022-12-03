ea674da6adea83379baeac53bb17abcc89a9fd17
vibovenkat123 <vibovenkat@gmail.com>
2022-12-03 10:24:44 -0600

    feat (lang svelte): convert zach's calendar page to svelte

---
+++ src/routes/calendar/+page.svelte
@@ -0,0 +1,10 @@
+  <svelte:head>
+    <meta charset="UTF-8">
+    <title>Calendar</title>
+    <link rel="stylesheet" href="/css/calendar.css">
+  </svelte:head>
+  <body>
+  <div id="calendar"></div>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min.js"></script>
+    <script src="/js/calendar.js"></script>
+  </body>
