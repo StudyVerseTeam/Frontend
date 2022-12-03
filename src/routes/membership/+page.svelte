075444b7e51c9cc736ff3889a263f79678025d0b
vibovenkat123 <vibovenkat@gmail.com>
2022-12-03 10:31:45 -0600

    feat (lang svelte): convert zach's membership page to svelte

--- src/routes/membership/+page.svelte
+++ src/routes/membership/+page.svelte
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html style="font-size: 16px;" lang="en"><head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta charset="utf-8">
-    <meta name="keywords" content="Pricing Plans, Free, $310, $530">
-    <meta name="description" content="">
-    <title>Home</title>
-    <link rel="stylesheet" href="main.css" media="screen">
-    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
-    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
-    
-    
-    <script type="application/ld+json">{
-		"@context": "http://schema.org",
-		"@type": "Organization",
-		"name": "",
-		"logo": "images/default-logo.png"
-}</script>
+<svelte:head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8">
+<meta name="keywords" content="Pricing Plans, Free, $310, $530">
+<meta name="description" content="">
+<title>Home</title>
+<link rel="stylesheet" href="/css/membership.css" media="screen">
+<link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,500,500i,600,600i,700,700i,800,800i">
+<link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i">
     <meta name="theme-color" content="#242943">
     <meta property="og:title" content="Home">
     <meta property="og:type" content="website">
-  </head>
+
+</svelte:head>
+   
+    
   <body class="u-body u-xl-mode" data-lang="en">
     <section class="u-align-center u-clearfix u-palette-1-dark-3 u-section-1" id="carousel_56ec">
       <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
@@ -182,4 +176,4 @@
       </div>
     </section>
   
-</body></html>
+</body>
