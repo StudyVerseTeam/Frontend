14d836885eb8a938c67e4cfe590fbe68c08263cb
vibovenkat123 <vibovenkat@gmail.com>
2022-12-08 18:06:21 -0600

    feat: add inter and epilogoue font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,20 @@
 <svelte:head>
     <link rel="icon" href="/images/favicon.ico">
 </svelte:head>
+<style global>
+  @import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400;500;600;800&display=swap');
+  @import url('https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400;500;600;800&family=Inter:wght@200;400;600&display=swap');
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Epilogue" !important;
+    font-weight: 400 !important;
+  }
+  p {
+    font-family: "Inter" !important;
+  }
+</style>
 <slot />
